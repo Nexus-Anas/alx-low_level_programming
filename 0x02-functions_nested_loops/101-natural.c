a0dbf6c5edd382c6a016abec9f218616190dc0c8
The-Vigilante <anas.vigilante@gmail.com>
2023-02-21 13:45:50 +0100

    0x02 T13 done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: int
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			b += a;
+		}
+		a++;
+	}
+	printf("%d\n", b);
+	return (0);
+}
