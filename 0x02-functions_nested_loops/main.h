7662f05724105ec9e1b3bdb88838170e6626e807
The-Vigilante <anas.vigilante@gmail.com>
2023-02-21 11:28:56 +0100

    0x02 T0 done

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+void alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int i);
+
+int _isalpha(int i);
+
+int _print_sign(int i);
+
+int _abs(int);
+
+int _print_last_digit(int);
+
+void _hours(void);
+
+void _times_table(void);
+
+int _add(int, int);
+
+void _print_to_98(int i);
