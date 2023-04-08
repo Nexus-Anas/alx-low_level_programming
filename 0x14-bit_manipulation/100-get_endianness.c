487e67122551b97f318a92e5f4bd35c8ae721c78
The-Vigilante <anas.vigilante@gmail.com>
2023-04-08 17:33:07 +0000

    0x14 advanced task 1 done and main.h updated

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - This func checks the endianness
+ *
+ * Return: returns 0 if big, else, 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int endianness = 1;
+	char *p = (char*) &endianness;
+
+	if (!*p)
+		return (0);
+	return (1);
+}
